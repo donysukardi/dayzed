6ee570aa25b7f25d5a762ad3c96f60c99817c364
Dony Sukardi <donysukardi@gmail.com>
2018-06-13 20:11:43 +0800

    Beautify example

--- stories/examples/Datepicker.js
+++ stories/examples/Datepicker.js
@@ -3,28 +3,68 @@ import glamorous from 'glamorous';
 import { DatePicker, MultiDatePicker } from '../../src/index';
 import { monthNamesFull, weekdayNamesShort } from './calendarUtils';
 
-let Calendar = glamorous.div({
+const Calendar = glamorous.div({
   maxWidth: 800,
   margin: '0 auto',
-  textAlign: 'center'
+  fontFamily: 'sans-serif'
 });
 
-let Month = glamorous.div({
-  display: 'inline-block',
-  width: '50%',
-  padding: '0 10px 30px',
-  boxSizing: 'border-box'
+const Month = glamorous.div({
+  display: 'table',
+  borderCollapse: 'collapse',
+  borderSpacing: 0,
+  userSelect: 'none',
+  margin: '0 1rem',
+  marginTop: '1rem'
 });
 
-const dayOfMonthStyle = {
-  display: 'inline-block',
-  width: 'calc((100% / 7) - 4px)', // make allowance for active border
+const Caption = glamorous.div({
+  padding: '0 .5rem',
+  display: 'table-caption',
+  textAlign: 'left',
+  marginBottom: '.5rem',
+  fontSize: '1.15rem',
+  fontWeight: '500'
+});
+
+const Weekdays = glamorous.div({
+  marginTop: '1rem',
+  display: 'table-header-group'
+});
+
+const WeekdaysRow = glamorous.div({
+  display: 'table-row'
+});
+
+const Weekday = glamorous.div({
+  display: 'table-cell',
+  padding: '.5rem',
+  fontSize: '.875em',
+  textAlign: 'center',
+  color: '#8b9898'
+});
+
+const Body = glamorous.div({
+  display: 'table-row-group'
+});
+
+const Week = glamorous.div({
+  display: 'table-row'
+});
+
+const DayCell = glamorous.div({
+  display: 'table-cell'
+});
+
+const dayStyle = {
+  padding: '.5rem',
   border: 'none',
-  margin: '2px' // make allowance for active border
+  verticalAlign: 'middle',
+  width: '100%'
 };
 
-let DayOfMonth = glamorous.button(
-  dayOfMonthStyle,
+const Day = glamorous.button(
+  dayStyle,
   ({ selected, unavailable, today, currentMonth }) => {
     let background = today ? 'cornflowerblue' : 'transparent';
     background = selected ? 'purple' : background;
@@ -34,7 +74,7 @@ let DayOfMonth = glamorous.button(
   }
 );
 
-let DayOfMonthEmpty = glamorous.div(dayOfMonthStyle, {
+const DayEmpty = glamorous.div(dayStyle, {
   background: 'transparent'
 });
 
@@ -98,52 +138,68 @@ const Datepicker = props => {
                 </button>
               </div>
               {calendars.map(calendar => (
-                <Month key={`${calendar.month}${calendar.year}`}>
-                  <div data-test="monthYear">
-                    {monthNamesFull[calendar.month]} {calendar.year}
-                  </div>
-                  {weekdayNames.map((weekday, idx) => (
-                    <DayOfMonthEmpty
-                      key={`${calendar.month}${calendar.year}${weekday}`}
-                      {...(idx === 0 ? { 'data-test': 'firstDayOfWeek' } : {})}
-                    >
-                      {weekday}
-                    </DayOfMonthEmpty>
-                  ))}
-                  <div data-test="calendarDates">
-                    {calendar.weeks.map((week, windex) =>
-                      week.map((dateObj, index) => {
-                        let key = `${calendar.month}${
-                          calendar.year
-                        }${windex}${index}`;
-                        if (!dateObj) {
-                          return <DayOfMonthEmpty key={key} />;
-                        }
-                        let {
-                          date,
-                          selected,
-                          selectable,
-                          today,
-                          prevMonth,
-                          nextMonth
-                        } = dateObj;
-                        return (
-                          <DayOfMonth
-                            key={key}
-                            {...getDateProps({
-                              dateObj
-                            })}
-                            selected={selected}
-                            unavailable={!selectable}
-                            currentMonth={!prevMonth && !nextMonth}
-                            today={today}
-                          >
-                            {selectable ? date.getDate() : 'X'}
-                          </DayOfMonth>
-                        );
-                      })
-                    )}
-                  </div>
+                <Month key={`${calendar.month}${calendar.year}`} role="grid">
+                  <Caption role="heading">
+                    <div data-test="monthYear">
+                      {monthNamesFull[calendar.month]} {calendar.year}
+                    </div>
+                  </Caption>
+                  <Weekdays role="rowGroup">
+                    <WeekdaysRow role="row">
+                      {weekdayNames.map((weekday, idx) => (
+                        <Weekday
+                          key={`${calendar.month}${calendar.year}${weekday}`}
+                          {...(idx === 0
+                            ? { 'data-test': 'firstDayOfWeek' }
+                            : {})}
+                        >
+                          {weekday}
+                        </Weekday>
+                      ))}
+                    </WeekdaysRow>
+                  </Weekdays>
+                  <Body data-test="calendarDates" role="rowgroup">
+                    {calendar.weeks.map((week, windex) => (
+                      <Week role="row">
+                        {week.map((dateObj, index) => {
+                          let key = `${calendar.month}${
+                            calendar.year
+                          }${windex}${index}`;
+                          if (!dateObj) {
+                            return (
+                              <DayCell key={key}>
+                                <DayEmpty />
+                              </DayCell>
+                            );
+                          }
+                          let {
+                            date,
+                            selected,
+                            selectable,
+                            today,
+                            prevMonth,
+                            nextMonth
+                          } = dateObj;
+                          return (
+                            <DayCell key={key}>
+                              <Day
+                                {...getDateProps({
+                                  dateObj
+                                })}
+                                selected={selected}
+                                unavailable={!selectable}
+                                currentMonth={!prevMonth && !nextMonth}
+                                today={today}
+                                role="gridcell"
+                              >
+                                {selectable ? date.getDate() : 'X'}
+                              </Day>
+                            </DayCell>
+                          );
+                        })}
+                      </Week>
+                    ))}
+                  </Body>
                 </Month>
               ))}
             </Calendar>
